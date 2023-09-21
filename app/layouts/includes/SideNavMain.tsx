f1feb7f670d8481f1e94a673cdaf244bd04aba13
Gabriel Dantas <gabrieldantas@Gabriels-MacBook-Air.local>
2023-09-20 22:10:02 -0300

    implements TopNav and other components

---
+++ app/layouts/includes/SideNavMain.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+
+function SideNavMain() {
+    const pathname = usePathname();
+
+  return (
+    <>
+        <div>
+            
+        </div>
+    </>
+  )
+}
+
+export default SideNavMain
