aa938f5d3f805ce35e22648a63f5d7e61d169442
Gabriel Dantas <gabrieldantas@Gabriels-MacBook-Air.local>
2023-09-26 19:41:17 -0300

    implements likes, comments on post

--- app/types.tsx
+++ app/types.tsx
@@ -4,6 +4,20 @@ export interface RandomUsers {
   image: string;
 }
 
+export interface Like {
+  id: string;
+  user_id: string;
+  post_id: string;
+}
+
+export interface Comment {
+  id: string;
+  user_id: string;
+  post_id: string;
+  text: string;
+  created_at: string;
+}
+
 export interface PostWithProfile {
   id: string;
   user_id: string;
@@ -21,6 +35,10 @@ export interface PostWithProfile {
 // COMPONENT TYPES
 
 export interface PostMainCompTypes {
+  post: PostWithProfile;
+}
+
+export interface PostMainLikesCompTypes {
   post: PostWithProfile;
 }
 
