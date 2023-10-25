929dafff1eb7b977607d4908cb9de71999aa56a1
Gabriel Dantas <gabrieldantas@Dantas-MacBook-Air.local>
2023-10-25 12:23:28 -0300

    implements appwrite endpoints

---
+++ libs/AppWriteClient.tsx
@@ -0,0 +1,11 @@
+import { Account, Client, ID, Databases, Query, Storage } from 'appwrite';
+
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('65392c76260c11d98645');
+
+    const account = new Account(client);
+    const database = new Databases(client);
+    const storage = new Storage(client);
+
+export { client, account, database, storage, Query, ID };
